ccb112ff140c2ce224e692772c26a707fa4eb331
Zacharia <ndunguzachary24@gmail.com>
2022-08-16 23:37:49 +0300

    commit tree 975de07c087ec3e5b559873af46d86f2a62f0e59

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet (name = "Naureen")
+    puts "Hello, #{name}!"
+end
+greet("Jimmy")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default()
+
+def add(num1, num2)
+    return  num1 + num2
+end
+add(4, 6)
+
+def halves(num1)
+    return  num1/2
+end 
+halves(6)
+
